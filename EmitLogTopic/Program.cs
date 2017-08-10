9a42360b6d42667d2439824f9de1a93c9f5a9a58
jesus Mendez <j.mendez@caribemedia.com.do>
2017-08-10 14:37:47 -0400

    +Adding and complete Unit 5

---
+++ EmitLogTopic/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmitLogTopic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //Direct Logs
+                channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+                Console.WriteLine(" Press Ctrl + C to exit.");
+                while (true) //write type of routing key for receiver processed
+                {
+                    Console.WriteLine("Write Topic Name:");
+                    var severity = Console.ReadLine();//GetMessage(args);
+                    Console.WriteLine("Write Message Inf:");
+                    var message = Console.ReadLine();
+                    var body = Encoding.UTF8.GetBytes(message);
+                    //Publish severals situations like INFO.severety etc.., WARNING, ERROR, ETC.
+                    channel.BasicPublish(exchange: "topic_logs",
+                                         routingKey: severity,
+                                         basicProperties: null,
+                                         body: body);
+                    //Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+        }
+    }
+}
---
+++ ReceiveLogsTopic/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiveLogsTopic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+
+                var queueName = channel.QueueDeclare().QueueName;
+
+                if (args.Length < 1)
+                {
+                    Console.Error.WriteLine("Usage: {0} binding_key",
+                                       Environment.GetCommandLineArgs()[0]);
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                    Environment.ExitCode = 1;
+                    return;
+                }
+                else
+                {
+                    foreach (var severity in args)
+                    {
+                        channel.QueueBind(queue: queueName,
+                                      exchange: "topic_logs",
+                                      routingKey: severity);
+                    }
+                    Console.WriteLine(" [*] Waiting for severity.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] {0}", message);
+                    };
+                    channel.BasicConsume(queue: queueName,
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
